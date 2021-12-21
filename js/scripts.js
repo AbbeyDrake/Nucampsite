e749be44aac0711dc11b5d5c40236590358d7da3
Abbey Drake <drakeam1189@gmail.com>
2021-12-21 10:37:19 -0700

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//BELOW: ready method - when the page loads and is ready, run what's inside of code block. First, sets carousel's interval to 2000ms. Then uses jQuery method called click which sets up an event handler to run pause and cycle functions when the buttons are clicked
+
 $(function(){
     $(".carousel").carousel( {interval: 2000 });
     $("#carouselButton").click(function(){
