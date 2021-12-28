d901380f2c8104ab56a0e9a84a140517365536a9
Abbey Drake <drakeam1189@gmail.com>
2021-12-27 19:37:42 -0700

    Week 4 Workshop

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 //BELOW: ready method - when the page loads and is ready, run what's inside of code block. First, sets carousel's interval to 2000ms. Then uses jQuery method called click which sets up an event handler to run pause and cycle functions when the buttons are clicked
 
-$(function(){
-    $(".carousel").carousel( {interval: 2000 });
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+
     $("#carouselButton").click(function(){
-        if($("#carouselButton").children("i").hasClass("fa-pause")){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
-        }else {
+        } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
-    
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
 });
+
+
